5cefe9ae6333017b82d06ba1039017be427b0a05
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2025-11-25 15:18:45 +0530

    implement user routes for fetching, updating, and deleting user data

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express'
+
+const router = Router()
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'got users' })
+})
+
+router.get('/:id', (req, res) => {
+  res.status(200).json({ message: 'got user' })
+})
+
+router.put('/:id', (req, res) => {
+  res.status(201).json({ message: 'updated user' })
+})
+
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ message: 'deleted user' })
+})
+
+export default router
