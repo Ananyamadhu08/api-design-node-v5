fc7389a913606eb18f80a373dde7fd97713bfcb7
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2025-12-03 18:46:04 +0530

    add morgan middleware for HTTP request logging

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import userRoutes from './routes/userRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
 import helmet from 'helmet'
 import cors from 'cors'
+import morgan from 'morgan'
+import { isTest } from '../env.ts'
 
 const app = express()
 
@@ -11,6 +13,11 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTest(),
+  })
+)
 
 app.get('/health', (req, res) => {
   res.status(200).json({
