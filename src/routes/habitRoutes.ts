2824a8871d545df8eb72fb8a1f9c979c28fda4ea
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2025-12-08 12:33:23 +0530

    update habitRoutes to use getUserHabits controller for retrieving user habits

--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { authenticateToken } from '../middleware/auth.ts'
-import { createHabit } from '../controllers/habitController.ts'
+import { createHabit, getUserHabits } from '../controllers/habitController.ts'
 import { z } from 'zod'
 import { validateBody } from '../middleware/validation.ts'
 
@@ -16,9 +16,7 @@ const router = Router()
 
 router.use(authenticateToken)
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'got habits' })
-})
+router.get('/', getUserHabits)
 
 router.get('/:id', (req, res) => {
   res.status(200).json({ message: 'got one habit' })
